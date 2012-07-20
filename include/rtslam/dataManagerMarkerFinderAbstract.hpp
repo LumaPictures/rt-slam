57d206570a3d85af588825af8bbe6238c24b76ac
Paul Molodowitch <paulm@luma-pictures.com>
2012-07-20 11:10:35 -0700

    dataManagerMarkerFinderAbstract: make class pure virtual; some indentation fixes

--- include/rtslam/dataManagerMarkerFinderAbstract.hpp
+++ include/rtslam/dataManagerMarkerFinderAbstract.hpp
@@ -23,21 +23,21 @@ namespace jafar {
 		*/
 		struct Marker
 		{
-				size_t id;
-				// Jafar standard translation/quat - ie tx,ty,tz,qw,qx,qy,qz
-				jblas::vec7 pose;
+			size_t id;
+			// Jafar standard translation/quat - ie tx,ty,tz,qw,qx,qy,qz
+			jblas::vec7 pose;
 
-				ublas::vector_range<jblas::vec7>
-				translation()
-				{
-					return subrange(pose, 0,3);
-				}
+			ublas::vector_range<jblas::vec7>
+			translation()
+			{
+				return subrange(pose, 0,3);
+			}
 
-				ublas::vector_range<jblas::vec7>
-				quaternion()
-				{
-					return subrange(pose, 3,7);
-				}
+			ublas::vector_range<jblas::vec7>
+			quaternion()
+			{
+				return subrange(pose, 3,7);
+			}
 		};
 
 		typedef boost::shared_ptr<Marker> MarkerPtr;
@@ -64,7 +64,7 @@ namespace jafar {
 				}
 				void processKnown(raw_ptr_t data, double date_limit = -1.) {
 				}
-				void detectNew(raw_ptr_t data);
+				void detectNew(raw_ptr_t data) = 0;
 
 				// TODO: eventually, support multiple-marker tracking, and
 				// convert this over to return a Marker list
